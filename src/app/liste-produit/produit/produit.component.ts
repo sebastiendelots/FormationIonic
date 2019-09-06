91c73349b8cfb16dd874ce9df2e2f62d94656704
ecole6 <ecole6@opt.nc>
2019-09-06 11:51:42 +1100

    change img of docker

--- src/app/liste-produit/produit/produit.component.ts
+++ src/app/liste-produit/produit/produit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Produit } from '../../model/produit';
 import { DataService } from '../../service/data.service';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-produit',
@@ -21,8 +21,9 @@ export class ProduitComponent implements OnInit {
   constructor(
     private dataService: DataService,
     private route: ActivatedRoute,
-    private navCtrl: NavController
-    ) { }
+    private navCtrl: NavController,
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe((res) => {
@@ -35,11 +36,21 @@ export class ProduitComponent implements OnInit {
     produit.categorie = item.categorie;
     produit.nom = item.nom;
     produit.id = item.id;
-    this.dataService.postProduit(produit).subscribe();
+    this.dataService.postProduit(produit).subscribe(() => this.presentAlert());
   }
 
   return() {
     this.navCtrl.pop();
- }
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Produit ' + this.item.id,
+      subHeader: 'Message de confirmation',
+      message: 'Le produit ' + this.item.nom + ' a été ajouté !',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 
 }
